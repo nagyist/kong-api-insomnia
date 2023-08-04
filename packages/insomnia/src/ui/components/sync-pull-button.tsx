Kong/insomnia

Add support for multiple remote projects per Team (#6009)
* update fetching of teams

* create and delete remote projects

* backend project sync using the new teamProjectId in the API

* indicator

* use team api to get organization data

* handle logged out state

* remove remote from duplicate modal

* remaining insomnia fetch fixes

* fix ts issues

* fix ts log issue

* fix ts error

* fix ts update

* fix tests

* remove log

* add types to insomniaFetch

* named arguments in vcs

---------

Co-authored-by: jackkav <jackkav@gmail.com>

packages/insomnia/src/ui/components/sync-pull-button.tsx
@@ -27,7 +27,7 @@ export const SyncPullButton: FC<Props> = props => {
     try {
       // Clone old VCS so we don't mess anything up while working on other projects
       await newVCS.checkout([], branch);
-      await newVCS.pull([], project.remoteId);
+      await newVCS.pull({ candidates: [], teamId: project.parentId, teamProjectId: project._id });
     } catch (err) {
       showError({
         title: 'Pull Error',

nagyist/kong-api-insomnia

packages/insomnia/src/ui/components/sync-pull-button.tsx
@@ -28,7 +28,7 @@ export const SyncPullButton: FC<Props> = props => {
     try {
       // Clone old VCS so we don't mess anything up while working on other projects
       await newVCS.checkout([], branch);
-      await newVCS.pull([], activeProject.remoteId);
+      await newVCS.pull({ candidates: [], teamId: activeProject.parentId, teamProjectId: activeProject._id });
     } catch (err) {
       showError({
         title: 'Pull Error',